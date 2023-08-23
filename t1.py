950c9f6f33ed62d4f1f7b7b1d17d8a51d863c0f4
MashirosPlanC <108707877+MashirosPlanC@users.noreply.github.com>
2023-08-23 11:27:14 -0400

    Create t1.py

---
+++ t1.py
@@ -0,0 +1,255 @@
+#Q1 45.Jump Game II
+# You are given a 0-indexed array of integers nums of length n. You are initially positioned at nums[0].
+
+# Each element nums[i] represents the maximum length of a forward jump from index i. In other words, if you are at nums[i], you can jump to any nums[i + j] where:
+
+# 0 <= j <= nums[i] and
+# i + j < n
+# Return the minimum number of jumps to reach nums[n - 1]. The test cases are generated such that you can reach nums[n - 1].
+
+ 
+
+# Example 1:
+
+# Input: nums = [2,3,1,1,4]
+# Output: 2
+# Explanation: The minimum number of jumps to reach the last index is 2. Jump 1 step from index 0 to 1, then 3 steps to the last index.
+# Example 2:
+
+# Input: nums = [2,3,0,1,4]
+# Output: 2
+ 
+
+# Constraints:
+
+# 1 <= nums.length <= 104
+# 0 <= nums[i] <= 1000
+# It's guaranteed that you can reach nums[n - 1].
+
+class Solution:
+  def jump(self, nums):
+    answer = 0
+    n = len(nums)
+    cur_end = 0
+    cur_far = 0
+
+    for i in range(n - 1):
+      cur_far = max(cur_far, i + nums[i])
+      if i == cur_end:
+        answer += 1
+        cur_end = cur_far
+
+    return answer
+
+#Q2 189. Rotate Array
+# Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
+
+ 
+
+# Example 1:
+
+# Input: nums = [1,2,3,4,5,6,7], k = 3
+# Output: [5,6,7,1,2,3,4]
+# Explanation:
+# rotate 1 steps to the right: [7,1,2,3,4,5,6]
+# rotate 2 steps to the right: [6,7,1,2,3,4,5]
+# rotate 3 steps to the right: [5,6,7,1,2,3,4]
+# Example 2:
+
+# Input: nums = [-1,-100,3,99], k = 2
+# Output: [3,99,-1,-100]
+# Explanation: 
+# rotate 1 steps to the right: [99,-1,-100,3]
+# rotate 2 steps to the right: [3,99,-1,-100]
+ 
+
+# Constraints:
+
+# 1 <= nums.length <= 105
+# -231 <= nums[i] <= 231 - 1
+# 0 <= k <= 105
+class Solution:
+  def rotate(self, nums, k):
+    n = len(nums)
+    k = k % n
+    nums[:] = nums[-k:] + nums[:-k]
+
+#Q3 122. Best Time to Buy and Sell Stock II
+# You are given an integer array prices where prices[i] is the price of a given stock on the ith day.
+
+# On each day, you may decide to buy and/or sell the stock. You can only hold at most one share of the stock at any time. However, you can buy it then immediately sell it on the same day.
+
+# Find and return the maximum profit you can achieve.
+
+ 
+
+# Example 1:
+
+# Input: prices = [7,1,5,3,6,4]
+# Output: 7
+# Explanation: Buy on day 2 (price = 1) and sell on day 3 (price = 5), profit = 5-1 = 4.
+# Then buy on day 4 (price = 3) and sell on day 5 (price = 6), profit = 6-3 = 3.
+# Total profit is 4 + 3 = 7.
+# Example 2:
+
+# Input: prices = [1,2,3,4,5]
+# Output: 4
+# Explanation: Buy on day 1 (price = 1) and sell on day 5 (price = 5), profit = 5-1 = 4.
+# Total profit is 4.
+# Example 3:
+
+# Input: prices = [7,6,4,3,1]
+# Output: 0
+# Explanation: There is no way to make a positive profit, so we never buy the stock to achieve the maximum profit of 0.
+ 
+
+# Constraints:
+
+# 1 <= prices.length <= 3 * 104
+# 0 <= prices[i] <= 104
+class Solution:
+  def maxProfit(self, prices):
+    profit = 0
+    for i in range(len(prices) - 1):
+      if prices[i] <= prices[i + 1]:
+        profit += prices[i + 1] - prices[i]
+    return profit
+
+#Q4 151. Reverse Words in a String
+# Given an input string s, reverse the order of the words.
+
+# A word is defined as a sequence of non-space characters. The words in s will be separated by at least one space.
+
+# Return a string of the words in reverse order concatenated by a single space.
+
+# Note that s may contain leading or trailing spaces or multiple spaces between two words. The returned string should only have a single space separating the words. Do not include any extra spaces.
+
+ 
+
+# Example 1:
+
+# Input: s = "the sky is blue"
+# Output: "blue is sky the"
+# Example 2:
+
+# Input: s = "  hello world  "
+# Output: "world hello"
+# Explanation: Your reversed string should not contain leading or trailing spaces.
+# Example 3:
+
+# Input: s = "a good   example"
+# Output: "example good a"
+# Explanation: You need to reduce multiple spaces between two words to a single space in the reversed string.
+ 
+
+# Constraints:
+
+# 1 <= s.length <= 104
+# s contains English letters (upper-case and lower-case), digits, and spaces ' '.
+# There is at least one word in s.
+class Solution:
+  def reverseWords(self, s):
+    s = s.split()
+    s.reverse()
+    return " ".join(s)
+
+#Q5 289. Game of Life
+# According to Wikipedia's article: "The Game of Life, also known simply as Life, is a cellular automaton devised by the British mathematician John Horton Conway in 1970."
+
+# The board is made up of an m x n grid of cells, where each cell has an initial state: live (represented by a 1) or dead (represented by a 0). Each cell interacts with its eight neighbors (horizontal, vertical, diagonal) using the following four rules (taken from the above Wikipedia article):
+
+# Any live cell with fewer than two live neighbors dies as if caused by under-population.
+# Any live cell with two or three live neighbors lives on to the next generation.
+# Any live cell with more than three live neighbors dies, as if by over-population.
+# Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
+# The next state is created by applying the above rules simultaneously to every cell in the current state, where births and deaths occur simultaneously. Given the current state of the m x n grid board, return the next state.
+
+ 
+
+# Example 1:
+
+
+# Input: board = [[0,1,0],[0,0,1],[1,1,1],[0,0,0]]
+# Output: [[0,0,0],[1,0,1],[0,1,1],[0,1,0]]
+# Example 2:
+
+
+# Input: board = [[1,1],[1,0]]
+# Output: [[1,1],[1,1]]
+ 
+
+# Constraints:
+
+# m == board.length
+# n == board[i].length
+# 1 <= m, n <= 25
+# board[i][j] is 0 or 1.
+class Solution:
+    def gameOfLife(self, board: List[List[int]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        #new_board = [[0 for ij in range(len(board[0]))] for ix in range(len(board))]
+       
+        # ix, iy = (1, 1), (1, -1), (-1, 1), (-1, -1)
+        directions = [(1,0),(1,1),(1,-1),(0,-1),(0,1),(-1,0),(-1,1),(-1,-1)]
+
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                count = 0
+                for x, y in directions:
+                    if i + x < len(board) and i + x >= 0 and j + y >= 0 and j + y < len(board[0]) and abs(board[i + x][y + j]) == 1:
+                        count += 1
+                if board[i][j] == 0 and count == 3:
+                    board[i][j] = 2
+                elif board[i][j] == 1 and (count < 2 or count > 3):
+                    board[i][j] = -1
+                # elif board[i][j] == 1 and (count == 2 or count == 3):
+                #     new_board[i][j] = 1
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                board[i][j] = 1 if (board[i][j] > 0) else 0 
+        return board
+
+#Q6 20. Valid Parentheses
+# Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+# An input string is valid if:
+
+# Open brackets must be closed by the same type of brackets.
+# Open brackets must be closed in the correct order.
+# Every close bracket has a corresponding open bracket of the same type.
+ 
+
+# Example 1:
+
+# Input: s = "()"
+# Output: true
+# Example 2:
+
+# Input: s = "()[]{}"
+# Output: true
+# Example 3:
+
+# Input: s = "(]"
+# Output: false
+ 
+
+# Constraints:
+
+# 1 <= s.length <= 104
+# s consists of parentheses only '()[]{}'.
+class Solution:
+  def isValid(self, s):
+    stack = []
+    m = {")":"(", "]":"[", "}":"{"}
+    for char in s:
+      if char in m.values():
+        stack.append(char)
+      elif char in m.keys():
+        if stack == [] or m[char] != stack.pop():
+          return False
+      else:
+        return False
+    return stack == []
+
